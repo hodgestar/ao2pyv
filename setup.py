daad18e8d53eac29c270fd1da7e96c81f8167e46
Simon Cross <hodgestar@gmail.com>
2015-08-22 01:24:26 +0200

    Version 0.1.0.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(filename):
 
 setup(
     name='ao2pyv',
-    version='0.0.1a',
+    version='0.1.0',
     url='https://github.com/hodgestar/ao2pyv',
     license='BSD',
     description=(
